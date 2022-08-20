a696841f40067cdf7044fd01517f7e627a7af68e
Nee Buntoum <101156624+neebuntoum@users.noreply.github.com>
2022-08-20 00:04:35 -0400

    using the console done

---
+++ app.js
@@ -0,0 +1,33 @@
+// alert('Hello World')
+
+// log to console
+
+console.log('Hello World');
+console.log(123);
+console.log(true);
+console.log('Hello World');
+
+let greeting = 'Hello';
+console.log(greeting);
+
+console.log([1,2,3,4,5]);
+
+console.log({a:1, b:2});
+
+console.table({a:1, b:2});
+  
+console.error('This is an error');
+
+console.clear();
+
+console.warn('This is a warning');
+
+console.time('Hello');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+  console.log('Hello World');
+console.timeEnd('Hello')
+
